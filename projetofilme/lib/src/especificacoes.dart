ac48128f537cd9820b43e9ea43f0de0e10462deb
blackmem712 <pedrotivictor712@gmail.com>
2023-04-29 21:38:30 -0400

    atualizações 29/04 21:38

---
+++ projetofilme/lib/src/especificacoes.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Especifica extends StatelessWidget {
+  const Especifica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Fixed Tabs'),
+            automaticallyImplyLeading: false,
+            backgroundColor: const Color(0xff5808e5),
+            bottom: const TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(text: 'filme resenha', icon: Icon(Icons.favorite)),
+                Tab(text: 'critica', icon: Icon(Icons.search)),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Center(child: Text('filme resenha')),
+              Center(child: Text('critica')),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ projetofilme/lib/src/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Inicial extends StatelessWidget {
+  const Inicial({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("inicial"),
+          ),
+          body: GridView.count(
+            scrollDirection: Axis.vertical,
+            crossAxisCount: 2,
+            padding: const EdgeInsets.only(top: 10.0),
+            mainAxisSpacing: 16.0,
+            childAspectRatio: 0.64,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/especificacoes');
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                    child: Column(
+                      children: const [
+                        Image(
+                          image: AssetImage(''),
+                        ),
+                        Text(
+                          "livrooo",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ))
+            ],
+          )),
+    );
+  }
+}
